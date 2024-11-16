52b0214da672155078c9e060495e54684ecde75a
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-11-16 17:41:21 +0530

    task random number maths

---
+++ Day 11/index.js
@@ -0,0 +1,15 @@
+function randomNumber() {
+    return Math.floor(Math.random() * 6) + 1
+}
+function playDice() {
+    let result1 = randomNumber();
+    let result2 = randomNumber();
+    let sum = result1 + result2;
+    if (sum === 7 || sum === 8) {
+        console.log("you win")
+    } else {
+        console.log("you lose")
+    }
+}
+
+playDice();
