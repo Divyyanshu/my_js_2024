f93f1e678556a5e29199e4f015ae23bf25d07241
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-11-18 13:06:01 +0530

    add day 11

--- Day 11/index.js
+++ Day 11/index.js
@@ -5,11 +5,12 @@ function playDice() {
     let result1 = randomNumber();
     let result2 = randomNumber();
     let sum = result1 + result2;
-    if (sum === 7 || sum === 8) {
-        console.log("you win")
-    } else {
-        console.log("you lose")
-    }
+    // if (sum === 7 || sum === 8) {
+    //     console.log("you win")
+    // } else {
+    //     console.log("you lose")
+    // }
+    sum === 8 || sum === 13 ? console.log("you win") : console.log("you lose")
 }
 
 playDice();
