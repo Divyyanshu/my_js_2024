5017a5943f0cb25021a4b4cee0451d86ebfe377b
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-11-26 18:32:58 +0530

    day 17 day 18 add

---
+++ Day18/index.js
@@ -0,0 +1,47 @@
+// const willYouPassedInExam = new Promise ((resolve,reject)=>{
+//     resolve();
+//     reject();
+// })
+
+// console.log(willYouPassedInExam)
+
+// function Mydetails(){
+//     return new Promise((res,rej)=>{
+//         if(Math.random() > 0.5){
+//             res("Promise successful");
+//         }else{
+//             rej("Promise failed");
+//         }
+//     })
+// }
+
+// Mydetails().then((data)=>{
+//    console.log(data)
+// }).catch((error)=>{
+//    console.log(error)
+// })
+
+// console.log(Mydetails())
+
+
+function fetchCountry(url){
+    const countryName = ["india" , "sri lanka" , "usa"];
+    return new Promise((res ,rej)=>{
+        if(url === 'www.myapi.com'){
+            res(countryName)
+        }else{
+            rej('wrong api')
+        }
+  
+    })
+}
+
+fetchCountry('www.myapi.com').then((data)=>{
+    data.forEach((country) => {     //FOR ARRAY LOOP ALL VALUES
+        const countryData = document.createElement("div");
+        countryData.textContent = country;
+        document.body.append(countryData)
+    });
+}).catch((err)=>{
+console.log(err)
+})
