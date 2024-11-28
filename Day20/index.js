369fba08806a5fe37b6ab95f92e977d19a9f1645
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-11-28 18:29:59 +0530

    this keyword / scope

---
+++ Day20/index.js
@@ -0,0 +1,35 @@
+const train = {
+    trainName: "Jaipur - Udaipur",
+    book: function (passangerName, trainNo) {
+        console.log(`${passangerName} has booked a ticket on ${this.trainName}(${trainNo})`)
+    }
+}
+
+train.book("Divyasnhu Tailor", "687286")
+
+const train2 = {
+    book: function (passangerName, trainNo, trainName) {
+        console.log(`${passangerName} has booked a ticket on ${trainName}(${trainNo})`)
+    }
+}
+
+train2.book("Divyasnhu Tailor", 687286, "jaipur-mumbai")
+
+
+// this object using this.binding fu ction ,methods
+
+function printName() {
+    console.log(this.firstName)
+}
+
+
+// making two object different diffrent name
+const person1 = {
+    firstName: "divyanshu"
+}
+const person2 = {
+    firstName: "mayank"
+}
+
+
+printName.call(person2)
